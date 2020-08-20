ccd3e6a872b72d24559625ff21393b5cfea8d453
Pranav Rao <pranavrao145@gmail.com>
2020-08-20 11:47:39 -0400

    completed 2019 J1 in C++

---
+++ C++/2019/Winning Score.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int apoints[3], bpoints[3];
+
+    for (int a=0; a < 3; a++) {
+        cin >> apoints[a];
+    }
+
+    for (int a=0; a < 3; a++) {
+        cin >> bpoints[a];
+    }
+    
+    int ascore = apoints[0] * 3 + apoints[1] * 2 + apoints[2];
+    int bscore = bpoints[0] * 3 + bpoints[1] * 2 + bpoints[2];
+
+    if (ascore > bscore) {
+        cout << "A" << endl;
+    }
+    else if (bscore > ascore) {
+        cout << "B" << endl;
+    }
+    else {
+        cout << "T" << endl;
+    }
+}
