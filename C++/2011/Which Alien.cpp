b08c98f61bd7fb7104b3868d918f12e3c3601234
Pranav Rao <pranavrao145@gmail.com>
2020-09-08 16:14:04 -0400

    added 2011 J1 in C++

---
+++ C++/2011/Which Alien.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "How many antennas?" << endl;
+    int antennas;
+    cin >> antennas;
+
+    cout << "How many eyes?" << endl;
+    int eyes;
+    cin >> eyes;
+
+    if (antennas >= 3 && eyes <= 4)
+        cout << "TroyMartian" << endl;
+
+    if (antennas <= 6 && eyes >= 2)
+        cout << "VladSaturnian" << endl;
+
+    if (antennas <= 2 && eyes <= 3)
+        cout << "GraemeMercurian" << endl;
+}
