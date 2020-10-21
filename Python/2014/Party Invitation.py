1c434cd59236e6322b0ee5a5d6ff273780264bcd
Pranav Rao <pranavrao145@gmail.com>
2020-10-21 19:19:24 -0400

    optimized 2014 J4

--- Python/2014/Party Invitation.py
+++ Python/2014/Party Invitation.py
@@ -1,14 +1,14 @@
 # J4
 
 if __name__ == "__main__":
-    k = int(input())
+    k = int(input())  # total number of friends
     m = int(input())  # number of rounds
 
-    friendslist = [i for i in range(1, k + 1)]
+    friendslist = [i for i in range(1, k + 1)] # [1,2,3,4,5..10]
 
     for i in range(m):
         friendslist = list(filter(lambda a: a != 0, friendslist))
-        r = int(input())
+        r = int(input()) # 2
         for g in range(len(friendslist)):
             if (g + 1) % r == 0:
                 friendslist[g] = 0
