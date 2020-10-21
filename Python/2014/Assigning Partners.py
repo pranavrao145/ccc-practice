039177c16440d3cc5dba8352228363585d585311
Pranav Rao <pranavrao145@gmail.com>
2020-10-21 17:03:39 -0400

    optimized 2014 J4 and commented 2014 J5, both in Python

--- Python/2014/Assigning Partners.py
+++ Python/2014/Assigning Partners.py
@@ -8,17 +8,17 @@ b = input().split()
 def balanced(a, b):
     partner1 = ''
     partner2 = ''
-    for i in range(len(a)):
-        if a[i] == b[i]:
+    for i in range(n):
+        if a[i] == b[i]: # goes through the first list and grabs the partner of the person in question for each loop
             return False
         else:
-            partner1 = b[i]
-        for g in range(len(b)):
+            partner1 = b[i] 
+        for g in range(n): # goes through the second list to find the person in question and grabs their partner from there
             if b[g] == a[i]:
                 partner2 = a[g]
-        if partner1 != partner2:
+        if partner1 != partner2: # if the person in question does NOT have the same partner in both lists, retrurns False right away 
             return False
-    return True
+    return True # if nothing fails and nobody is partnered with themselves and all partners are consistent, returns true
 
 
 if balanced(a, b):
--- Python/2014/Party Invitation.py
+++ Python/2014/Party Invitation.py
@@ -1,34 +1,19 @@
 # J4
 
-k = int(input())
-friendslist = []
+if __name__ == "__main__":
+    k = int(input())
+    m = int(input())  # number of rounds
 
-for i in range(1, k + 1):
-    friendslist.append(i)
+    friendslist = [i for i in range(1, k + 1)]
 
-# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# [1, 3, 5, 7, 9]
-# [1, 3, 7, 9]
+    for i in range(m):
+        friendslist = list(filter(lambda a: a != 0, friendslist))
+        r = int(input())
+        for g in range(len(friendslist)):
+            if (g + 1) % r == 0:
+                friendslist[g] = 0
 
-m = int(input())  # number of rounds
+    friendslist = list(filter(lambda a: a != 0, friendslist))
 
-
-def RemAll0s(L):
-    newlist = []
-    for i in L:
-        if i != 0:
-            newlist.append(i)
-    return newlist
-
-
-for i in range(m):
-    friendslist = RemAll0s(friendslist)
-    r = int(input())
-    for g in range(1, len(friendslist) + 1):
-        if g % r == 0:
-            friendslist[g - 1] = 0
-
-friendslist = RemAll0s(friendslist)
-
-for i in friendslist:
-    print(i)
+    for friend in friendslist:
+        print(friend)
