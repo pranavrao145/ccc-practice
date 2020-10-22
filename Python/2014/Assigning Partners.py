a266e34e7d483d4731b2d9f5e91b77c204e972d0
Pranav Rao <pranavrao145@gmail.com>
2020-10-21 20:20:30 -0400

    optimized 2014 J5

--- Python/2014/Assigning Partners.py
+++ Python/2014/Assigning Partners.py
@@ -1,27 +1,24 @@
 # J5
 
-n = int(input())
-a = input().split()
-b = input().split()
+num = int(input())
+ 
+a, b, parters1, partners2, repeat = input().strip().split(), input().strip().split(), dict(), dict(), False
+
+if __name__ == "__main__":
+    for i in range(num):
+        if a[i] == b[i]:
+            repeat = True
+            break
+
+        parters1[a[i]] = b[i]
+        partners2[b[i]] = a[i]
+
+    if repeat or parters1 != partners2:
+        print("bad")
+    else:
+        print("good")
 
 
-def balanced(a, b):
-    partner1 = ''
-    partner2 = ''
-    for i in range(n):
-        if a[i] == b[i]: # goes through the first list and grabs the partner of the person in question for each loop
-            return False
-        else:
-            partner1 = b[i] 
-        for g in range(n): # goes through the second list to find the person in question and grabs their partner from there
-            if b[g] == a[i]:
-                partner2 = a[g]
-        if partner1 != partner2: # if the person in question does NOT have the same partner in both lists, retrurns False right away 
-            return False
-    return True # if nothing fails and nobody is partnered with themselves and all partners are consistent, returns true
 
 
-if balanced(a, b):
-    print('good')
-else:
-    print('bad')
+
