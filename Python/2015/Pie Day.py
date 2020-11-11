73bea06ed4859eec022c0bbb6af1cfc5dd771552
Pranav Rao <pranavrao145@gmail.com>
2020-11-10 21:41:06 -0500

    added 2015 J5 in Python and C++

--- Python/2015/Pie Day.py
+++ Python/2015/Pie Day.py
@@ -2,12 +2,12 @@ n = int(input())
 k = int(input())
 ans = 0 
 
-def pi(pieces, people, min):
+def pi(pieces, people, minimum):
     global ans
     if people == 1:
         ans += 1
         return
-    for i in range(min, pieces//people + 1):
+    for i in range(minimum, pieces//people + 1):
         pi(pieces - i, people - 1, i)
 
 pi(n, k, 1)
