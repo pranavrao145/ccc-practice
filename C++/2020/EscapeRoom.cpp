5bc17ae4250498042396a91f7f53466b33f47254
Pranav Rao <pranavrao145@gmail.com>
2021-07-21 16:14:54 -0400

    changed escape room in cpp

--- C++/2020/EscapeRoom.cpp
+++ C++/2020/EscapeRoom.cpp
@@ -1,71 +1,69 @@
 // J5 - doesn't work for all
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
-using namespace std;
 
 int M, N;
-vector<vector<int>> matrix;
+std::vector<std::vector<int>> matrix;
+std::vector<std::pair<int, int>> visited;
 bool found = false;
-vector<pair<int, int>> visited;
 
 void DFS(int d, int a);
 
+std::vector<std::pair<int, int>> factors(int num) {
+    std::vector<std::pair<int, int>> final_factors;
 
-vector<pair<int, int>> factors(int num) {
-    vector<pair<int, int>> final_factors;
-
-    for (int i = 1; i < num + 1; i++) {
-        if (num % i == 0) {
-            if (i <= M && num / N <= N) {
-                final_factors.emplace_back(i, num / i);
-            }
-        }
+  for (int i = 1; i < num + 1; i++) {
+    if (num % i == 0) {
+      if (i <= M && num / N <= N) {
+        final_factors.emplace_back(i, num / i);
+      }
     }
+  }
 
-    return final_factors;
+  return final_factors;
 }
 
 int main() {
-    cin >> M >> N;
+    std::cin >> M >> N;
 
-    matrix.resize(M);
+  matrix.resize(M);
 
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            int num;
-            cin >> num;
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      int num;
+      std::cin >> num;
 
-            matrix[i].push_back(num);
-        }
+      matrix[i].emplace_back(num);
     }
+  }
 
-    DFS(1, 1);
+  DFS(1, 1);
 
-    if (found) cout << "yes" << endl;
-    else cout << "no" << endl;
+  if (found)
+    std::cout << "yes" << std::endl;
+  else
+    std::cout << "no" << std::endl;
 
-    return 0;
+  return 0;
 }
 
-
 void DFS(int d, int a) {
-    if (d == M && a == N) {
-        found = true;
-        return;
-    }
-
-    vector<pair<int, int>> facts = factors(matrix[d - 1][a - 1]);
-
-    if (!found) {
-        for (auto fact : facts) {
-            if (find(visited.begin(), visited.end(), fact) == visited.end()) {
-                visited.push_back(fact);
-                DFS(fact.first, fact.second);
-            }
-        }
-    }
+  if (d == M && a == N) {
+    found = true;
     return;
+  }
+
+  std::vector<std::pair<int, int>> facts = factors(matrix[d - 1][a - 1]);
+
+  if (!found) {
+    for (auto fact : facts) {
+      if (find(visited.begin(), visited.end(), fact) == visited.end()) {
+        visited.emplace_back(fact);
+        DFS(fact.first, fact.second);
+      }
+    }
+  }
+  return;
 }
